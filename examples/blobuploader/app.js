33d99b4c969d9f18a0d7aff94eb6ac66f8bb49b6
Marius Obert <marius.obert@sap.com>
2019-03-22 11:35:26 +0100

    make example ready for Cloud Foundry deployment

--- examples/blobuploader/app.js
+++ examples/blobuploader/app.js
@@ -1,30 +1,35 @@
-// 
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
-var express = require('express');
-var expressLayouts = require('express-ejs-layouts');
-var path = require('path');
-var azure = require('azure-storage');
-var formidable = require('formidable');
-var helpers = require('./helpers.js');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const azure = require('azure-storage');
+const formidable = require('formidable');
+const helpers = require('./helpers.js');
+const cfenv = require('cfenv');
+
+// const credentials = cfenv.getAppEnv().getService('myAzureStorage').credentials;
+const credentials = require('./credentials')
 
 var app = express();
 
 // Global request options, set the retryPolicy
-var blobClient = azure.createBlobService('UseDevelopmentStorage=true').withFilter(new azure.ExponentialRetryPolicyFilter());
+var blobClient = azure.createBlobService(credentials.storageAccountName,
+  credentials.accessKey, credentials.primaryBlobServiceEndPoint).withFilter(new azure.ExponentialRetryPolicyFilter());
 var containerName = 'webpi';
 
 //Configuration
@@ -118,7 +123,7 @@ app.post('/Delete/:id', function (req, res) {
 blobClient.createContainerIfNotExists(containerName, function (error) {
   if (error) {
     console.log(error);
-  } else { 
+  } else {
     setPermissions();
   }
 });
@@ -128,7 +133,7 @@ function setPermissions() {
   blobClient.setContainerAcl(containerName, null, options, function (error) {
     if (error) {
       console.log(error);
-    } 
+    }
   });
 }
 
